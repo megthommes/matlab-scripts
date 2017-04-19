c9b122e2bf6bd2b94f03a5f7e6b1ab115ed48a8e
Meghan Thommes <meghan.thommes@gmail.com>
2017-04-19 11:39:48 -0400

    dFBA: Renamed variables for clarity

--- FBA/dFBA.m
+++ FBA/dFBA.m
@@ -1,11 +1,11 @@
-function [time,biomass,exchMets_amt,exchMets_flux,exchMets_names] = dFBA(model,media_names,media_amt,varargin)
+function [time,biomass,flux,exchMets_amt,exchMets_names] = dFBA(model,media_names,media_amt,varargin)
 %dFBA Solve a dynamic flux balance analysis problem for multiple organisms
 %
 % Solves LP problems of the form: max/min c'*v
 %                                 subject to S*v = b
 %                                            lb <= v <= ub
-% [time,biomass,exchMets_amt,exchMets_flux,flux,exchMets_names] = dFBA(model,media_names,media_amt)
-% [time,biomass,exchMets_amt,exchMets_flux,flux,exchMets_names] = dFBA(model,media_names,media_amt,biomass_0,dt,N,volume,Km,Vmax,max_biomass)
+% [time,biomass,flux,exchMets_amt,exchMets_names] = dFBA(model,media_names,media_amt)
+% [time,biomass,flux,exchMets_amt,exchMets_names] = dFBA(model,media_names,media_amt,biomass_0,dt,N,volume,Km,Vmax,max_biomass)
 %
 %REQUIRED INPUTS
 % The model structure must contain the following fields:
@@ -37,11 +37,11 @@ function [time,biomass,exchMets_amt,exchMets_flux,exchMets_names] = dFBA(model,m
 %OUTPUT
 % time: time vector [hr]
 % biomass: cell vector of biomass {organism}[gCDW]
-% exchMets_amt: matrix of extracellular media concentrations (time x mets) [mmol]
-% exchMets_flux: cell matrix of media fluxes {organism}(time x mets) [mmol/gCDW/hr]
 % flux: cell matrix of all fluxes {organism}(time x rxns) [mmol/gCDW/hr]
-% exchMets_names: vector of all the extraceullular media concentration names
+% exchMets_amt: matrix of extracellular metabolite concentrations (time x mets) [mmol]
+% exchMets_names: extracellular metabolite names
 %
+% Meghan Thommes 03/29/2017 - Renamed variables for clarity
 % Meghan Thommes 03/02/2016 - Added volume as a variable
 % Meghan Thommes 03/01/2016 - Updated Initializing Metabolites
 % Meghan Thommes 09/03/2015
@@ -52,13 +52,17 @@ if (nargin < 3)
     error('myfuns:dFBA:NotEnoughInputs', ...
         'Not enough inputs: need model, media_names, and media_amt');
 elseif (nargin==3)
-    if ~isstruct(model)
-        error('myfuns:dFBA:IncorrectType', ...
-            '"model" needs to be a structure');
-    elseif ~isfield(model,'S') || ~isfield(model,'b') || ~isfield(model,'c') || ~isfield(model,'lb') || ~isfield(model,'ub')
-        error('myfuns:dFBA:IncorrectType', ...
-            '"model" needs "S", "b", "c", "lb", and "ub" fields');
-    elseif ~iscellstr(media_names)
+    for ii = 1:numel(model)
+        if ~isstruct(model{ii})
+            error('myfuns:dFBA:IncorrectType', ...
+                '"model" needs to be a structure');
+        elseif ~isfield(model{ii},'S') || ~isfield(model{ii},'b') || ~isfield(model{ii},'c') || ~isfield(model{ii},'lb') || ~isfield(model{ii},'ub')
+            error('myfuns:dFBA:IncorrectType', ...
+                '"model" needs "S", "b", "c", "lb", and "ub" fields');
+        end
+    end
+    
+    if ~iscellstr(media_names)
         error('myfuns:dFBA:IncorrectType', ...
             '"media_names" needs to be a string cell');
     elseif ~isnumeric(media_amt)
@@ -70,8 +74,8 @@ end
 %% Define Parameter Values
 
 % Set defaults for optional inputs
-optargs = {1E-5.*ones(length(model),1), 0.1, 100, 1, ... % biomass_0, dt, N, volume
-    0.01.*ones(length(model),1), -10.*ones(length(model),1), 2.2E-4.*ones(length(model),1)}; %  Km, Vmax, max_biomass
+optargs = {1E-5.*ones(numel(model),1), 0.1, 100, 1, ... % biomass_0, dt, N, volume
+    0.01.*ones(numel(model),1), -10.*ones(numel(model),1), 2.2E-4.*ones(numel(model),1)}; %  Km, Vmax, max_biomass
 % Skip new inputs if they are empty
 newVals = cellfun(@(x) ~isempty(x), varargin);
 % Overwrite parameters set by varargin
@@ -86,29 +90,28 @@ biomass_thresh = 1E-9;
 %% Define Extracellular Metabolites
 
 exchMets_names = {}; % extracellular metabolites
-excRxns = cell(length(model)); % pre-allocate
-excMets = cell(length(model)); % pre-allocate
-for numModel = 1:length(model)
+exchRxns_idxInModel = cell(numel(model)); % pre-allocate
+exchMets_idxInModel = cell(numel(model)); % pre-allocate
+for numModel = 1:numel(model)
     % Find Exchange Reactions and Metabolites
-    [excRxns{numModel},~] = findExchRxns(model{numModel});
-    [excMets{numModel},~] = findExchMets(model{numModel});
+    [exchRxns_idxInModel{numModel},~] = findExchRxns(model{numModel});
+    [exchMets_idxInModel{numModel},~] = findExchMets(model{numModel});
     
     % Define All the Extracellular Metabolites
-    mets = setdiff(model{numModel}.mets(excMets{numModel}),exchMets_names); % model mets NOT in extracellular mets
+    mets = setdiff(model{numModel}.mets(exchMets_idxInModel{numModel}),exchMets_names); % model mets NOT in extracellular mets
     exchMets_names = [mets; exchMets_names]; % add to list
 end
 
 %% Match Exchange Reactions to Extracellular Metabolites
 
-exchMets_idx = cell(size(model));
-ec_rxns_idx = cell(size(model));
+exchMets_idxInMedia = cell(size(model));
+exchRxns_idxInMedia = cell(size(model));
 
-for numModel = 1:length(model)
+for numModel = 1:numel(model)
     [isMet, index] = ismember(model{numModel}.mets,exchMets_names);
-    exchMets_idx{numModel} = index(isMet); % extracellular mets in model mets - index into extracellular mets
+    exchMets_idxInMedia{numModel} = index(isMet); % extracellular mets in model mets - index into extracellular mets
     clear isMet index
-    [ec_rxns_idx{numModel},~] = findRxnsFromMets(model{numModel},exchMets_names(exchMets_idx{numModel})); % model rxns of extracellular mets - index into model rxns
-    ec_rxns_idx{numModel} = find(ec_rxns_idx{numModel} == 1);
+    [exchRxns_idxInMedia{numModel},~] = findRxnsFromMets(model{numModel},exchMets_names(exchMets_idxInMedia{numModel})); % model rxns of extracellular mets - index into model rxns
 end
 
 %% Set Initial Metabolites
@@ -120,48 +123,50 @@ exchMets_amt_0(exchMets_names_idx) = media_amt(media_names_idx);
 %% Initalize Vectors
 
 time = (0:dt:N*dt)'; % time
-exchMets_amt = zeros(N+1,length(exchMets_amt_0)); % metabolite abundance [mmol]
+exchMets_amt = zeros(N+1,numel(exchMets_amt_0)); % metabolite abundance [mmol]
 exchMets_amt(1,:) = exchMets_amt_0; % metabolite abundance at t=0 [mmol]
-biomass = cell(length(model),1); % biomass [gCDW] 
-exchMets_flux = cell(length(model),1); % metabolite exchange flux [mmol/gCDW*hr]
-Vuptake_max = cell(length(model),1); % maximum metabolite flux [mmol/gCDW*hr]
-Vmax = cell(length(model),1); % Michaelis-Menten max reaction rate [mmol/gCDW/hr]
-Km = cell(length(model),1); % Michaelis-Menten constant [mM]
+biomass = cell(numel(model),1); % biomass [gCDW] 
+flux = cell(numel(model),1); % metabolite exchange flux [mmol/gCDW*hr]
+Vuptake_max = cell(numel(model),1); % maximum metabolite flux [mmol/gCDW*hr]
+Vmax = cell(numel(model),1); % Michaelis-Menten max reaction rate [mmol/gCDW/hr]
+Km = cell(numel(model),1); % Michaelis-Menten constant [mM]
 
 % Set for each organism
-for numModel = 1:length(model)
+for numModel = 1:numel(model)
     biomass{numModel} = zeros(N+1,1); % biomass [gCDW]
     biomass{numModel}(1) = biomass_0(numModel); % biomass at t=0 [gCDW]
-    exchMets_flux{numModel} = zeros(N+1,length(exchMets_amt_0));  % metabolite exchange flux [mmol/gCDW*hr]
-    Vuptake_max{numModel} = model{numModel}.lb(ec_rxns_idx{numModel})'; % maximum metabolite flux [mmol/gCDW*hr]
-    Vmax{numModel} = vmax(numModel).*ones(1,length(exchMets_amt_0(exchMets_idx{numModel}))); % Michaelis-Menten max reaction rate [mmol/gCDW/hr]
-    Km{numModel} = km(numModel).*ones(1,length(exchMets_amt_0(exchMets_idx{numModel}))); % Michaelis-Menten constant [mM]
+    flux{numModel} = zeros(N+1,numel(model{numModel}.rxns));  % metabolite exchange flux [mmol/gCDW*hr]
+    Vuptake_max{numModel} = model{numModel}.lb(exchRxns_idxInMedia{numModel})'; % maximum metabolite flux [mmol/gCDW*hr]
+    Vmax{numModel} = vmax(numModel).*ones(1,numel(exchMets_amt_0(exchMets_idxInMedia{numModel}))); % Michaelis-Menten max reaction rate [mmol/gCDW/hr]
+    Km{numModel} = km(numModel).*ones(1,numel(exchMets_amt_0(exchMets_idxInMedia{numModel}))); % Michaelis-Menten constant [mM]
 end
 
 %% Perform Dynamic Flux Balance Analysis
 
 for n = 1:N
     % Randomize Order Organisms Uptake Metabolites
-    eatOrder = randperm(length(model),length(model));
+    eatOrder = randperm(numel(model),numel(model));
     
     for numModel = eatOrder   
         % Calculate Uptake Rate for Exchange Reactions
-        v_uptake = (Vmax{numModel}.*exchMets_amt(n,exchMets_idx{numModel}))./(Km{numModel}.*volume + exchMets_amt(n,exchMets_idx{numModel})); % v = v_max*([S]/(Km + [S]))
+        v_uptake = (Vmax{numModel}.*exchMets_amt(n,exchMets_idxInMedia{numModel}))./(Km{numModel}.*volume + exchMets_amt(n,exchMets_idxInMedia{numModel})); % v = v_max*([S]/(Km + [S]))
         
         % Check if Calculated Uptake is Greater than Max Uptake
         v_uptake(v_uptake < Vuptake_max{numModel}) = Vuptake_max{numModel}(v_uptake < Vuptake_max{numModel}); % if greater than max, set to max uptake
         v_uptake(v_uptake > -1E-9 & v_uptake < 0) = 0;
-        model{numModel}.lb(excRxns{numModel}) = v_uptake; % specify new bounds
+        model{numModel}.lb(exchRxns_idxInModel{numModel}) = v_uptake; % specify new bounds
         
         % Calculate Growth Rate
         FBA_solution = FBA(model{numModel});
         
         if strcmp(FBA_solution.status,'OPTIMAL') % need optimal solution
             growth_rate = FBA_solution.objectiveValue;
-            exchMets_flux{numModel}(n+1,exchMets_idx{numModel}) = FBA_solution.fluxes(excRxns{numModel});
+            flux{numModel}(n+1,:) = FBA_solution.fluxes';
+            exchMets_flux = FBA_solution.fluxes(exchRxns_idxInModel{numModel})';
         else
             growth_rate = 0;
-            exchMets_flux{numModel}(n+1,exchMets_idx{numModel}) = zeros(1,length(ec_rxns_idx{numModel}));
+            flux{numModel}(n+1,:) = zeros(1,numel(model{numModel}.rxns));
+            exchMets_flux = zeros(1,numel(exchRxns_idxInMedia{numModel}));
         end
         
         % Calculate Biomass and External Metabolite Amounts
@@ -170,10 +175,10 @@ for n = 1:N
         biomass{numModel}(biomass{numModel} < biomass_thresh) = 0;
         
         % External Metabolite Amounts
-        exchMets_amt(n+1,:) = exchMets_amt(n,:) + exchMets_flux{numModel}(n,:).*biomass{numModel}(n).*dt; % S(t+dt) = S(t) + v*X(t)*dt
+        exchMets_amt(n+1,:) = exchMets_amt(n,:) + exchMets_flux.*biomass{numModel}(n).*dt; % S(t+dt) = S(t) + v*X(t)*dt
         exchMets_amt(exchMets_amt < media_thresh) = 0; % no negative concentrations
         
-        clear v_uptake FBA_solution growth_rate
+        clear v_uptake FBA_solution growth_rate exchMets_flux
     end
     clear eatOrder
 end
